6b36b812454187fa9c9ae2c034687d40e36f97bc
alvaroormeno <alvaro@ormeno.org>
2022-03-11 14:59:39 -0500

    added css

--- src/page-template.js
+++ src/page-template.js
@@ -76,12 +76,15 @@ const createCards = (team) => {
 };
 
 module.exports = (team) => {
-  `<!DOCTYPE html>
+  return `
+  <!DOCTYPE html>
     <html lang="en">
     
     <head>
         <meta charset="UTF-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="template.css">
+        
         <title>Team Profile Generator</title>
     </head>
     
